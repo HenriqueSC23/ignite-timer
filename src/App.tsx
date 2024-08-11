abcc36fdd2a38efb585c8659565513eb69a1c32a
Henrique <henriq2303@gmail.com>
2024-08-11 10:52:31 -0300

    change hashrouter location

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { HashRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <HashRouter>
-        <CyclesContextProvider>
-          <Router />
-        </CyclesContextProvider>
-      </HashRouter>
+      <CyclesContextProvider>
+        <Router />
+      </CyclesContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   )
